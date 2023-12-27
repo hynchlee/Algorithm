5ca11bad4c318fec6ed298747bd8fa1b59b44ca3
hynchlee <102397695+hynchlee@users.noreply.github.com>
2023-12-27 10:08:23 +0900

    [level unrated] Title: 대문자와 소문자, Time: 12.83 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(char x : my_string.toCharArray()){
+            if(Character.isLowerCase(x)){
+                answer += Character.toUpperCase(x);
+            }else{
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+}
