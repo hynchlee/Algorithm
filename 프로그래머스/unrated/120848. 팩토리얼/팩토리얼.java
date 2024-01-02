f5e0dc3d32754baeb0522860e9c8811503ba1949
hynchlee <102397695+hynchlee@users.noreply.github.com>
2024-01-02 11:07:47 +0900

    [level unrated] Title: 팩토리얼, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int mul = 1;
+        for(int i=1; i<=n; i++){
+            mul *= i;
+            if(mul==n){
+                answer = i;
+                break;
+            }else if(mul > n){
+                answer = i-1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
