277b7e8f35bb847650bb36f354c821fae9690938
hynchlee <102397695+hynchlee@users.noreply.github.com>
2024-01-03 09:16:05 +0900

    [level unrated] Title: 7의 개수, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String arr = Arrays.toString(array);
+        for(int i=0; i<arr.length(); i++){
+            if(arr.charAt(i) == '7'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
