a7344cc01dbe510835c7ab26e33cd6def7b650bf
hynchlee <102397695+hynchlee@users.noreply.github.com>
2024-01-22 13:55:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,13 @@
 class Solution {
     public long solution(int n) {
-        long arr []= new long[2001];
-
-	    arr[1]=1;
-	    arr[2]=2;
-	 
-	    for(int i=3; i<2001; i++) {
-		    arr[i] = (arr[i-1]+arr[i-2])%1234567;			 
-	    }		 
-	        return arr[n];
+        long[] dp = new long[n+8];
+        dp[1] = 1;
+        dp[2] = 2;
         
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        
+        return dp[n];
     }
 }
