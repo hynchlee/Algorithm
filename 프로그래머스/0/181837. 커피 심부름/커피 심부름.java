60343f2100cedfea00e356d5cc14a8a8ea0b2e49
hynchlee <102397695+hynchlee@users.noreply.github.com>
2024-02-28 15:43:32 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,10 +2,10 @@ class Solution {
     public static int solution(String[] order) {
     	int answer = 0;
     	for(String str : order) {
-            if(str.contains("americano") || str.equals("anything")) {
-            	answer += 4500;
-            }else {
+            if(str.contains("cafelatte")) {
             	answer += 5000;
+            }else {
+            	answer += 4500;
             }
         }
         return answer;
