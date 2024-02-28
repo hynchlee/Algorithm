941f05f26d0e3049db03ca40d31d80d7f23a9938
hynchlee <102397695+hynchlee@users.noreply.github.com>
2024-02-28 11:24:53 +0900

    [level 0] Title: 세 개의 구분자, Time: 112.34 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,27 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String myStr) {
-        ArrayList<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        for(char c : myStr.toCharArray()){
-            if(c=='a'||c=='b'||c=='c'){
-                if(sb.length() != 0){
-                    list.add(sb.toString());
-                    sb.setLength(0);
-                    //sb = new StringBuilder();
-                }
-            }else{
-                sb.append(c);
-            }
-        }
-        if(sb.length() > 0){
-            list.add(sb.toString());
-        }
-        if(sb.length() == 0){
+    // public String[] solution(String myStr) {
+    //     ArrayList<String> list = new ArrayList<>();
+    //     StringBuilder sb = new StringBuilder();
+    //     for(char c : myStr.toCharArray()){
+    //         if(c=='a'||c=='b'||c=='c'){
+    //             if(sb.length() != 0){
+    //                 list.add(sb.toString());
+    //                 sb.setLength(0);
+    //                 //sb = new StringBuilder();
+    //             }
+    //         }else{
+    //             sb.append(c);
+    //         }
+    //     }
+    //     if(sb.length() > 0){
+    //         list.add(sb.toString());
+    //     }
+    //     if(sb.length() == 0){
+    //         return new String[] {"EMPTY"};
+    //     }else{
+    //         return list.toArray(new String[0]);
+    //     }
+    // }
+    public static String[] solution(String myStr) {
+    	String trim = myStr.trim();
+        String str = trim.replaceAll("[abc]+", " ");
+        String[] split = str.split("\\s+");
+
+        ArrayList<String> result = new ArrayList<String>();
+        for (String s : split) {
+			if(!s.isEmpty()) {
+				result.add(s);
+			}
+		}
+        
+        if (result.isEmpty()) {
             return new String[] {"EMPTY"};
-        }else{
-            return list.toArray(new String[0]);
+        } else {
+            return result.toArray(new String[0]);
         }
     }
 }
